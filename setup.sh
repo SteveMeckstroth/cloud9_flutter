28dfc9bf2dc90ff2a45fe8e74845beab01268d6e
meckstss <meckstss@gmail.com>
2020-03-03 21:19:29 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Downloading Android SDK"
+
+cd /opt
+sudo wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
+sudo tar -xvf android-sdk*-linux.tgz
+cd android-sdk-linux/tools
+sudo ./android update sdk --no-ui --filter platform,platform-tools
+
+echo "Setting Paths"
+sudo echo 'export PATH=$PATH:/opt/android-sdk-linux/platform-tools' >> /etc/profile.d/android.sh
+
+sudo echo 'export ANDROID_TOOLS=/opt/android-sdk-linux' >> /etc/profile.d/android.sh
+source /etc/profile.d/android.sh
